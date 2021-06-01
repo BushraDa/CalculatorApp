f0408cfe3a0793e7720155290328174624acd89f
bushrada <boushra.m.da@gmail.com>
2021-06-01 13:25:24 +0300

    Coding the basics operations

---
+++ js/main.js
@@ -0,0 +1,48 @@
+var firstOperand;
+var secondOperand;
+var operation;
+
+function calculate() {
+    value = $('.screen').text();
+    idx = value.indexOf(operation);
+    secondOperand = Number(value.substring(idx+1, value.length));
+    if (operation === '+') {
+        result = firstOperand + secondOperand;
+    } else if (operation === '-') {
+        result = firstOperand - secondOperand;
+    } else if (operation === '*') {
+        result = firstOperand * secondOperand;
+    } else if (operation === '/') {
+        result = firstOperand / secondOperand;
+    }
+  
+    $('.screen').text(result);
+}
+
+function operator(elem) {
+    firstOperand = Number($('.screen').text());
+    operation = elem.innerText;
+    updateScreen(elem);
+}
+
+function resetCalculator() {
+    $('.screen').text('0');
+}
+
+function deleteCalculator() {
+    value = $('.screen').text();
+    value = value.substring(0,value.length-1);
+    if (value.length != 0)
+        $('.screen').text(value);
+    else
+        $('.screen').text('0');
+}
+
+function updateScreen(elem) {
+    value = $('.screen').text();
+    newVal = elem.innerText;
+    if (value == 0)
+        $('.screen').text(newVal);
+    else
+        $('.screen').text(value + newVal);
+}
