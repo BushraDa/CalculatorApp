0668beddad61a41b4143a7be0290a50a39668694
bushrada <boushra.m.da@gmail.com>
2021-06-02 13:58:18 +0300

    fixing theme toggle

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@ var firstOperand;
 var secondOperand;
 var operation;
 
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme == 'theme2') {
+        $('#theme2')[0].checked = true;
+    } else if (currentTheme == 'theme3') {
+        $('#theme3')[0].checked = true;
+    }
+}
+
 function calculate() {
     value = $('.screen').text();
     idx = value.indexOf(operation);
@@ -46,3 +58,16 @@ function updateScreen(elem) {
     else
         $('.screen').text(value + newVal);
 }
+
+function setTheme(theme) {
+    if (theme.value == 'theme1') {
+        document.documentElement.setAttribute('data-theme', 'theme1');
+        localStorage.setItem('theme', 'theme1');
+    } else if (theme.value == 'theme2') {
+        document.documentElement.setAttribute('data-theme', 'theme2');
+        localStorage.setItem('theme', 'theme2');
+    } else if (theme.value == 'theme3') {
+        document.documentElement.setAttribute('data-theme', 'theme3');
+        localStorage.setItem('theme', 'theme3');
+    }
+  }
